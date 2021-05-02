e44c6c3e840b144b7ffc16fba876a254da97b0c9
gavinbarnard <gavin.r.barnard@gmail.com>
2021-05-02 03:38:46 +0000

    reverse block output

--- src/report.py
+++ src/report.py
@@ -42,7 +42,9 @@ def main():
 # temporary disable unsure I have the correct calculations in stats.py
     template = template.replace("<!-- SITENAME --!>", config_items['sitename'])
     template = template.replace("<!-- POOLLOGO --!>", config_items['pool_logo'])
-    blocks = blocks.replace("\n", "<br>")
+    blocks = blocks.split("\n")
+    blocks = blocks.reverse()
+    blocks = "<br>".join(blocks)
     template = template.replace("<!-- BLOCKLIST --!>", blocks)
     pool_hash = "Network HR vs Pool HR scale is roughly 1/{}".format(multi)
     template = template.replace("<!-- MULTI --!>", pool_hash)
